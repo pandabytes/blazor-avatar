96226bb5db8d95a51dd299516301bd8d43f668f5
Huy Ngo <huybngo@yahoo.com>
2024-03-27 16:24:14 -0700

    Add auto load attribute

--- src/Components/BaseComponent.cs
+++ src/Components/BaseComponent.cs
@@ -9,22 +9,50 @@ namespace Blazor.Avatar.Components;
 /// </summary>
 public abstract class BaseComponent : OwningComponentBase, IAsyncDisposable
 {
+  /// <summary>
+  /// Specify a field to be automatically injected a scoped service
+  /// during <see cref="OnInitialized"/>.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+  protected sealed class InjectScopeServiceAttribute : Attribute {}
+
+  /// <summary>
+  /// Specify a field whose type derives from <see cref="BaseJsModule"/>
+  /// and automatically load it during <see cref="OnInitializedAsync"/>.
+  /// This attribute is only valid on fields marked with <see cref="InjectScopeServiceAttribute"/>.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+  protected sealed class AutoLoadJsModuleAttribute : Attribute {}
+
   /// <inhereitdoc />
   protected override void OnInitialized()
   {
     base.OnInitialized();
-    var type = GetType();
-    
-    // Inject any scope service here
-    type
-      .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
-      .Where(field => field.GetCustomAttribute<InjectScopeServiceAttribute>() is not null)
-      .ToList()
-      .ForEach(field =>
-      {
-        var scopeService = ScopedServices.GetRequiredService(field.FieldType);
-        field.SetValue(this, scopeService);
-      });
+
+    foreach (var field in GetInjectScopeServiceFields())
+    {
+      // Inject any scope service here
+      var scopeService = ScopedServices.GetRequiredService(field.FieldType);
+      field.SetValue(this, scopeService);
+    }
+  }
+
+  /// <inhereitdoc />
+  protected override async Task OnInitializedAsync()
+  {
+    await base.OnInitializedAsync();
+
+    // Find fields that are marked with AutoLoadJsModule
+    // and their type must derive from BaseJsModule
+    var autoLoadFields = GetInjectScopeServiceFields()
+      .Where(field => field.GetCustomAttribute<AutoLoadJsModuleAttribute>() is not null)
+      .Where(field => typeof(BaseJsModule).IsAssignableFrom(field.FieldType));
+
+    foreach (var field in autoLoadFields)
+    {
+      var jsModule = (BaseJsModule)field.GetValue(this)!;
+      await jsModule.LoadModuleAsync();
+    }
   }
 
   /// <inhereitdoc />
@@ -34,4 +62,9 @@ public abstract class BaseComponent : OwningComponentBase, IAsyncDisposable
     await (ScopedServices as IAsyncDisposable)!.DisposeAsync();
     GC.SuppressFinalize(this);
   }
+
+  private IEnumerable<FieldInfo> GetInjectScopeServiceFields()
+    => GetType()
+         .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+         .Where(field => field.GetCustomAttribute<InjectScopeServiceAttribute>() is not null);
 }
--- src/Components/InjectScopeService.cs
+++
@@ -1,4 +0,0 @@
-namespace Blazor.Avatar.Components;
-
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-internal sealed class InjectScopeServiceAttribute : Attribute {}
